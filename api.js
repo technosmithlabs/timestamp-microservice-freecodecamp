854232b283b59136aaa798efa917e4c25cf4c3c8
Anshul Gautam <anshulgammy@gmail.com>
2020-05-02 15:31:27 +0530

    updated the code for timestamp service in api.js

--- api.js
+++ api.js
@@ -1,8 +1,22 @@
 var getWebService = function (app) {
+  app.get("/api/timestamp/", (req, res, next) => {
+    var reqDate = new Date();
+    res.json({ unix: reqDate.getTime(), utc: reqDate.toUTCString() });
+    next();
+  });
   app.get("/api/timestamp/:date_string", (req, res, next) => {
     var reqDateString = req.params.date_string;
-    var reqDate = new Date(reqDateString);
-    if (isNaN(reqDate.getTime())) {
+    var reqDate = null;
+    if (isNaN(reqDateString)) {
+      reqDate = new Date(reqDateString);
+    } else {
+      reqDate = new Date(parseInt(reqDateString));
+    }
+    if (reqDateString == null || reqDateString == "") {
+      reqDate = new Date();
+      res.json({ unix: reqDate.getTime(), utc: reqDate.toUTCString() });
+    } else if (isNaN(reqDate.getTime())) {
+      console.log(reqDate);
       res.json({ error: "Invalid Date" });
     } else {
       res.json({ unix: reqDate.getTime(), utc: reqDate.toUTCString() });
